86acd44c56f0d23a5b731e794106909b14116123
DongKonLee <dle0129@gmail.com>
2022-11-19 15:36:51 +0900

    LeetCode: Sqrt(x)
    - Level: Easy
    - Type: Impl

---
+++ Algorithm/src/LeetCode/Sqrt_x.java
@@ -0,0 +1,12 @@
+package LeetCode;
+
+public class Sqrt_x {
+    public static void main(String[] args) {
+        System.out.println(mySqrt(4));
+        System.out.println(mySqrt(8));
+    }
+
+    public static int mySqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
+}
