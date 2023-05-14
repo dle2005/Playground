74f9bc9fe537427e0aab5def82a91d9586533bac
DongKonLee <dle0129@gmail.com>
2023-05-14 17:57:37 +0900

    AutoIncrement default 값 설정 테스트

--- Classes/spring_jpa_basic/src/test/kotlin/com/example/spring_jpa_basic/autoincrement/AutoIncrementTest.kt
+++ Classes/spring_jpa_basic/src/test/kotlin/com/example/spring_jpa_basic/autoincrement/AutoIncrementTest.kt
@@ -4,10 +4,12 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 
 @DataJpaTest
 internal class AutoIncrementTest @Autowired constructor(
     private val testRepository: TestRepository,
+    private val testEntityManager: TestEntityManager,
 ) {
     @Test
     fun AutoIncrement기본값설정해도에러없음() {
@@ -15,5 +17,19 @@ internal class AutoIncrementTest @Autowired constructor(
         val savedEntity = testRepository.saveAndFlush(entity)
 
         assertThat(savedEntity.id).isNotEqualTo(0)
+    }
+
+    @Test
+    fun entityInformation_isNew_기본값검증() {
+        val entity1 = TestEntity()
+        val savedEntity1 = testRepository.saveAndFlush(entity1)
+
+        testEntityManager.clear()
+
+        val entity2 = TestEntity()
+        val savedEntity2 = testRepository.saveAndFlush(entity2)
+
+        assertThat(savedEntity1.id).isEqualTo(1)
+        assertThat(savedEntity2.id).isEqualTo(2)
     }
 }
